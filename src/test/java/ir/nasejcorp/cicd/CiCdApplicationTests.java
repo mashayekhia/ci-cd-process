49b58e6147ec7b597092fe4f0ed2cccfc490def7
Aghil Mashayekhi <aghil.mashayekhi@gmail.com>
2022-05-11 04:37:35 +0430

    Initialize ci-cd-process application

---
+++ src/test/java/ir/nasejcorp/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.nasejcorp.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
